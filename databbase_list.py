9e5752b1e6e2a867307037a8301c6c06dd3a0183
akrabatt <66750951+akrabatt@users.noreply.github.com>
2020-09-09 20:13:33 +0200

    new commit

---
+++ databbase_list.py
@@ -0,0 +1,38 @@
+import sqlite3
+import os
+import time
+
+data_base = '/test_data_base.db'
+table = 'clients_base'
+file = 'test.txt'
+
+
+def database_print_list(data_base, table, column_name = None):
+    con = sqlite3.connect('test_data_base.db')  # конектимся с БД
+    cur = con.cursor()  # создаём объект курсор для дальнейшей работы с ним
+    query_name = 'pragma table_info(' + table + ')'  # узнаем наименование колонок
+    cur.execute(query_name)  # выполняем query_name
+    column_des = cur.fetchall()  # получаем данные
+    if column_name is None:  # если имя колонки не задано
+        query = 'SELECT * FROM ' + table  # все записи из всек колонок из таблицы
+        cur.execute(query)
+        data = cur.fetchall()
+    print(data)
+    file_database = open('test.txt', 'w')
+    for i in range(len(data)):
+        file_database.write('\n')
+        # file_database.write(data[i][0])
+        for b in range(len(data[i])):
+            file_database.write(data[i][b])
+            file_database.write(' ')
+
+    file_database.close()
+    cur.close()
+    con.close()
+    os.startfile('test.txt')
+    time.sleep(1)
+    os.remove('test.txt')
+
+
+if __name__ == '__main__':
+    database_print_list(data_base, table)
--- test_version.py
+++ test_version.py
@@ -8,6 +8,7 @@ table = 'clients_base'
 print('введите 1 если хотите выполнить добавить клиента в базу')
 print('введите 2 если хотите просмотреть базу клиентов')
 print('введите 3 если хотите удалить клиента из базы')
+print('введите 4 если  хотите вывести список клиентской базы')
 
 num = input('введите цифру: ')
 num = int(num)
@@ -18,5 +19,7 @@ elif num == 2:
     request_from_database.request_bd(data_base=data_base, table=table)
 elif num == 3:
     delete_from_datatbase.delete_from_db()
+elif num ==4:
+    pass
 else:
     print('вы ввели неверное значение')
